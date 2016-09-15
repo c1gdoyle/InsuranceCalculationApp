b5c04c22cd59d90e2669276e20a91d171bea3927
c1gdoyle <c1gdoyle@gmail.com>
2016-09-15 22:50:02 +0100

    Unity application
    - added basic view and viewController

---
+++ Demo.InsuranceCalculationApp/BootStrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Unity;
+
+namespace Demo.InsuranceCalculationApp
+{
+    /// <summary>
+    /// Basic implemenation of <see cref="UnityBootstrapper"/>.
+    /// </summary>
+    public class BootStrapper : UnityBootstrapper
+    {
+        #region UnityBootstrapper Overrides
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<InsuranceCalculationAppShell>();
+        }
+
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+            App.Current.MainWindow = (InsuranceCalculationAppShell)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog.AddModule(null);
+        }
+        #endregion UnityBootstrapper Overrides
+
+    }
+}
---
+++ Demo.InsuranceCalculationApp/ViewControllers/InsuranceCalculationViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Presentation.Base;
+using Prism.Commands;
+
+namespace Demo.InsuranceCalculationApp.ViewControllers
+{
+    public class InsuranceCalculationViewController : ViewControllerBase
+    {
+        public InsuranceCalculationViewController()
+        {
+            InitialiseCommands();
+        }
+
+        private void InitialiseCommands()
+        {
+            ExitCommand = new DelegateCommand(() => NotifiyWindowShouldClose(null));
+        }
+
+        #region Properties
+        public string Title
+        {
+            get { return "Insurance Premium Calculator"; }
+        }
+        #endregion Properties
+
+        #region Commands
+        public DelegateCommand ExitCommand
+        {
+            get;
+            private set;
+        }
+        #endregion Commands
+    }
+}
